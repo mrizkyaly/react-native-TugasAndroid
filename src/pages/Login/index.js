0a3fa3d21289a1b3f3f5d33298a21cf18c7c90fd
Muhammad Rizky Aly <rizkyaly20.ra@gmail.com>
2021-11-14 12:14:03 +0700

    intregasi login page using axios

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useCallback, useEffect, useState} from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
+import {useToast} from 'react-native-toast-notifications';
 import ILLogo from '../../assets/img/pablita-success.png';
 import {Button, Input, Link} from '../../components/atoms';
 
 const Login = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [dataUser, setDataUser] = useState([]);
+  const [itemLogin, setItemLogin] = useState({});
+
+  const toast = useToast();
+
+  const getDataUser = useCallback(async () => {
+    const result = await axios.get('http://10.0.2.2:3000/users');
+    setDataUser(result);
+  }, []);
+
+  useEffect(() => {
+    getDataUser();
+  }, []);
+
   const onPress = () => {
-    navigation.navigate('UserProfile');
+    const data = {
+      email,
+      password,
+    };
+
+    if (data.email === '' || data.password === '') {
+      toast.show('Pastikan isi form dengan lengkap !!', {
+        type: 'danger',
+        placement: 'bottom',
+      });
+    } else {
+      dataUser.data.map(item => {
+        if (data.email === item.email && data.password === item.password) {
+          toast.show('Login Berhasil', {
+            type: 'success',
+            placement: 'top',
+          });
+          navigation.navigate('UserProfile');
+        }
+      });
+    }
   };
+
   return (
     <View style={styles.page}>
       <Text style={styles.title}>Welcome Back, Guys</Text>
@@ -15,8 +54,16 @@ const Login = ({navigation}) => {
         <Image source={ILLogo} style={styles.logo} />
       </View>
       <View style={styles.fillAuth}>
-        <Input placeholder="Email" />
-        <Input placeholder="Password" />
+        <Input
+          placeholder="Email"
+          value={email}
+          onChangeText={value => setEmail(value)}
+        />
+        <Input
+          placeholder="Password"
+          value={password}
+          onChangeText={value => setPassword(value)}
+        />
       </View>
       <Link
         title="Forget my password ?"
